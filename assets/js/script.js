47b5028cdb7472f42a3fda5e5d11c75412ab161e
khanhlc04 <laothucatkhanhle@gmail.com>
2024-12-18 16:25:14 +0700

    fouth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 //AOS
 AOS.init();
 //End AOS
+
+//swipper
+const swiper = new Swiper(".swipper-blog", {
+    slidesPerView: 1,
+    spaceBetween: 12,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 12
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+});
+//end swipper
